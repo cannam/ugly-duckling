f1f9f8ae6b47b644690adacb976bc4c60a968d45
Chris Cannam <cannam@all-day-breakfast.com>
2017-05-15 10:17:53 +0100

    Update necessary bootstrap worker file

--- bootstrap-feature-extraction-worker.js
+++ bootstrap-feature-extraction-worker.js
@@ -2,9 +2,10 @@
  * Created by lucas on 01/12/2016.
  */
 let window = {};
-importScripts('inline.bundle.js'); // provides webpackJsonp
+importScripts('/inline.bundle.js'); // provides webpackJsonp
 const webpackJsonp = window['webpackJsonp'];
-importScripts('scripts.bundle.js'); // needs webpackJsonp, hence above - provides RequireJS
-importScripts('polyfills.bundle.js'); // provides the compiled FeatureExtractionWorker
+importScripts('/scripts.bundle.js'); // needs webpackJsonp, hence above - provides RequireJS
+window = null; // now we have a reference to webpackJsonp, get rid of window
+importScripts('/polyfills.bundle.js'); // provides the compiled FeatureExtractionWorker
 
 new (require('feature-extraction-worker'))(self, requirejs);
